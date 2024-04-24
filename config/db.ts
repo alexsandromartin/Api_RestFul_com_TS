4cb595744e3e0f83284b8d5a22f83e35c43e8d71
Alexsandro Martins Alves <alexsandromartins.0172@gmail.com>
2024-04-24 10:37:11 -0300

    adicionando e integrando o banco de dados

---
+++ config/db.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import config from "config"
+
+async function connect(){
+
+    const dbUri = config.get<string>("dbUri")
+
+    try {
+
+        await mongoose.connect(dbUri)
+        console.log("Conectou ao banco de dados")
+        
+    } catch (e) {
+        console.log("Não foi possivel conectar ao banco de Dados")
+        console.log(`Erro: ${e}`)
+    }
+}
+
+export default connect
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-console.log("Deu bom!")
+//ENV variables
+require("dotenv").config()
+
+import express from "express"
+import config from "config"
+
+const app = express()
+
+//Json middleware
+app.use(express.json())
+
+//DB
+import db from "../config/db"
+
+//Routes
+import router from "./router"
+
+app.use("/api/", router)
+
+// app port 
+const port = config.get<number>("port")
+
+app.listen(3000, async () => {
+    await db()
+    console.log("A aplicação está funcionando na porta: 3000")
+})
