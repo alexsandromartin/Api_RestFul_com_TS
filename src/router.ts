65aaf173c5813487d88ab7c33d175cc1a99d3d8e
Alexsandro Martins Alves <alexsandromartins.0172@gmail.com>
2024-05-12 21:59:56 -0300

    middleware de validação de erros

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,11 @@
 import {Router, Response, Request} from "express"
 import { createMovie } from "./controllers/movieController"
 
+//validators
+import {validate} from "./middleware/hendleValidation"
+
 const router = Router()
 
 export default router.get("/test", (req:Request, res:Response) => {
     res.status(200).send("Api Working!")
-}).post("/movie", createMovie)
+}).post("/movie", validate, createMovie)
